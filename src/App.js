0ba3be55f85adf4c9d72cabb6163ea473577faf5
yuriya <namyuri@gmail.com>
2021-10-15 21:57:13 +0900

    chapter 3 done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from "react";
 
 class App extends React.Component{
   state = {
-    count: 0
+    isLoading: true
   };
-  up = () => {
-    this.setState(current => ({count: current.count + 1}))
-  };
-  down = () => {
-    this.setState(current => ({count: current.count - 1}))
-  };
-  componentDidMount(){
-    console.log("component did mount")
-  }
-  componentDidUpdate(){
-    console.log("component did update")
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
   render() {
+    const {isLoading} = this.state;
     return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.up}>Up</button>
-      <button onClick={this.down}>Down</button>
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
   };
 }
